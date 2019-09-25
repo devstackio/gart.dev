79f23990805748810d7f030365f04fc5afa38b64
devstackio <gartflores@gmail.com>
2019-09-24 20:37:21 -0400

    cleaned unused imports, basic navigation setup between home and about components from navigation component

--- web/src/component/Navigation.js
+++ web/src/component/Navigation.js
@@ -1,7 +1,5 @@
-import React, {Component} from 'react'
-import {Route,Link} from 'react-router-dom'
-import Home from './Home'
-import About from './About'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Navigation = ()=> {
   return(
