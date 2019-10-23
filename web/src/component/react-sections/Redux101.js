4bf4aeaccde9a56aa009d271e82fce3e6ee95c04
devstackio <gartflores@gmail.com>
2019-10-23 16:06:42 -0400

    updated redux 101 section

--- web/src/component/react-sections/Redux101.js
+++ web/src/component/react-sections/Redux101.js
@@ -11,13 +11,14 @@ const Redux101 = () => {
       <h3>Redux 101</h3>
       <Accordion expanded={expanded} setExpanded={setExpanded} id="1" 
       header = 'redux/configureStore.js'
-      content = 'Create new Shader (Outline Shader) and take code from <a href="https://wiki.unity3d.com/index.php/Silhouette-Outlined_Diffuse">from here</a>'
+      content = {`redux-immutable-state-invariant will throw error if trying to modify state in reducer, intended for dev use only
+      composeEnhancers are for adding redux developer tools`}
       code = {`import { createStore, applyMiddleware, compose } from 'redux';
       import rootReducer from './reducers';
       import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
       
       export default function configureStore(initialState) {
-        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+        const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
         return createStore(
           rootReducer, 
           initialState, 
@@ -25,11 +26,151 @@ const Redux101 = () => {
         );
       }
         `}
-      ></Accordion>
+      />
 
-      <Accordion expanded={expanded} setExpanded={setExpanded} id="2" header = 'Create Shader'
-      content = 'Create new Shader (Outline Shader) and take code from <a href="https://wiki.unity3d.com/index.php/Silhouette-Outlined_Diffuse">from here</a>'
-      ></Accordion>
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="2" 
+      header = 'actions/actionTypes.js'
+      content = ''
+      code = {`export const SOME_ACTION = "SOME_ACTION";`}
+      />
+
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="3" 
+      header = 'actions/componentActions.js'
+      content = ''
+      code = {`import * as types from "./actionTypes";
+
+      export function someAction(someData) {
+        return { type: types.SOME_ACTION, someData };
+      }
+        `}
+      />
+
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="4" 
+      header = 'reducers/someReducer.js'
+      content = ''
+      code = {`import * as types from '../actions/actionTypes';
+
+      export default function someReducer(state = [], action) {
+        switch(action.type) {
+          case types.SOME_ACTION:
+            return [
+              ...state, { ...action.course }
+            ]
+          default:
+            return state;
+        }
+      }
+        `}
+      />
+
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="5" 
+      header = 'reducers/index.js'
+      content = ''
+      code = {`import { combineReducers } from 'redux';
+      import courses from './courseReducer';
+      
+      const rootReducer = combineReducers({
+        courses
+      });
+      
+      export default rootReducer;
+        `}
+      />
+
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="6" 
+      header = 'root/index.js'
+      content = ''
+      code = {`import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux';
+
+const store = configureStore();
+
+render(
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>, 
+  document.getElementById("app"));
+        `}
+      />
+
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="7" 
+      header = 'SomeComponent.js'
+      content = ''
+      code = {`import React from 'react';
+      import { connect } from 'react-redux';
+      import { bindActionCreators } from 'redux';
+      import * as componentActions from '../../redux/actions/componentActions';
+      
+      class SomeComponent extends React.Component {
+        constructor(props) {
+          super(props);
+      
+          this.state = {
+            course: {
+              title: ""
+            }
+          }
+      
+        }
+      
+        handleChange = event => {
+          const course = { ...this.state.course, title: event.target.value};
+          this.setState({ course });
+        };
+      
+        handleSubmit = event => {
+          event.preventDefault();
+          this.props.actions.someAction(this.state.course);
+        }
+        render() {
+          return (
+            <form onSubmit={this.handleSubmit}>
+              <h2>Courses</h2>
+              <h3>Add Course</h3>
+              <input 
+                type="text" 
+                onChange={this.handleChange} 
+                value={this.state.course.title} />
+              <input type="submit" value="Save" />
+              { this.props.courses.map(course => (
+                <div key={course.title}>{course.title}</div>
+              ))}
+            </form>
+          )
+        }
+      }
+      
+      CoursesPage.propTypes = {
+        actions: PropTypes.object.isRequired,
+        courses: PropTypes.array.isRequired
+      }
+      
+      function mapStateToProps(state) {
+        return {
+          courses: state.courses
+        };
+      }
+      
+      function mapDispatchToProps(dispatch) {
+        return {
+          actions: bindActionCreators(componentActions, dispatch)
+        }
+      }
+      
+      export default connect(mapStateToProps, mapDispatchToProps)(SomeComponent);
+        `}
+      />
+
+      {/* <Accordion expanded={expanded} setExpanded={setExpanded} id="1" 
+      header = 'redux/configureStore.js'
+      content = 'redux-immutable-state-invariant will throw error if trying to modify state in reducer, intended for dev use only'
+      code = {`
+        `}
+      /> */}
+
+      
     </>
   )
 }
