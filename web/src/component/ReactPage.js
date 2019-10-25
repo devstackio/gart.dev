1bf0d78fc90712bb254924e06afc83b688b6459e
devstackio <gartflores@gmail.com>
2019-10-25 10:58:59 -0400

    updated reactPage with redirects101 and route101 sections

--- web/src/component/ReactPage.js
+++ web/src/component/ReactPage.js
@@ -3,7 +3,9 @@ import React from 'react'
 import Destructuring101 from 'component/react-sections/Destructuring101'
 import Fetch101 from 'component/react-sections/Fetch101'
 import Map101 from 'component/react-sections/Map101'
+import Redirects101 from 'component/react-sections/Redirects101'
 import Redux101 from 'component/react-sections/Redux101'
+import Route101 from 'component/react-sections/Route101'
 import Thunk101 from 'component/react-sections/Thunk101'
 import UseEffect101 from 'component/react-sections/UseEffect101'
 import UseState101 from 'component/react-sections/UseState101'
@@ -16,7 +18,9 @@ const ReactPage = () => {
       <Destructuring101 />
       <Fetch101 />
       <Map101 />
+      <Redirects101 />
       <Redux101 />
+      <Route101 />
       <Thunk101 />
       <UseEffect101 />
       <UseState101 />
