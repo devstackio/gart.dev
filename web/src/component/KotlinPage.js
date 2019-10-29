20db78d14ac6f1036dab2d9ba361565bb2a489fa
devstackio <gartflores@gmail.com>
2019-10-29 15:30:53 -0400

    added Classes101 section

--- web/src/component/KotlinPage.js
+++ web/src/component/KotlinPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Properties101 from 'component/kotlin-sections/Properties101'
+import Classes101 from 'component/kotlin-sections/Classes101'
 
 const KotlinPage = () => {
   return (
     <>
       <h1>Kotlin</h1>
       <Properties101 />
+      <Classes101 />
     </>
   )
 }
---
+++ web/src/component/kotlin-sections/Classes101.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Accordion from 'component/basic/Accordion'
+
+const Classes101 = () => {
+
+  const [ expanded, setExpanded ] = React.useState(false);
+
+  return (
+    <>
+      <h3>Classes 101</h3>
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="1" 
+      header = 'instantiation'
+      content = 'can omit the "new" keyword'
+      code = {`val a = Apple()
+        `}
+      />
+
+      <Accordion expanded={expanded} setExpanded={setExpanded} id="2" 
+      header = 'primary constructor'
+      content = 'basically a javascript function definition with property types'
+      code = {`
+      class Person(name: String, weight: Double) {
+        val name = name
+        var weight = weight
+      }
+
+      // declare and initialize properties in primary constructor
+
+      class Person(val name: String, var weight: Double) {
+        
+      })
+        `}
+      />
+
+      {/* <Accordion expanded={expanded} setExpanded={setExpanded} id="2" 
+      header = 'tt1'
+      content = ''
+      code = {`
+        `}
+      /> */}
+
+      
+    </>
+  )
+}
+
+export default Classes101;
